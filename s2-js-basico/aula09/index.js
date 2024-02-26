122ef50916b5b8cd6bd486ff35afe4c1b4d43494
Wesley Moraes <wesleycrz@hotmail.com>
2024-02-26 15:12:27 -0300

    var e let

---
+++ s2-js-basico/aula09/index.js
@@ -0,0 +1,5 @@
+var nome = "wesley"
+var nome = "Wesley"
+
+//Esse é o problema! Com o var é possível ficar redeclarando variável! No ES6 isso foi corrigido com a criação do let!
+//É um comportamento nã desejado!
