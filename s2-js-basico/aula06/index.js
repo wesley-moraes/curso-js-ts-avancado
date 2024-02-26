ef4096d6d32b1729d1cdf5f39a31bda90c015d09
Wesley Moraes <wesleycrz@hotmail.com>
2024-02-26 14:30:51 -0300

    Aula 06 - let

---
+++ s2-js-basico/aula06/index.js
@@ -0,0 +1,17 @@
+let nome = "Joao";
+var nome2 = "Maria";
+let nome3;
+/**
+ * Primeiro você declara, depois você inicializa => let declaraOValor; declaraOValor = "inicializa o valor"
+ * A virgula concatena as strings! 
+ * Existem palavras que são reservadas da própria lingaugem! Não se pode declarar variável let, por exemplo 
+ * Nomes de varáveis significativos! Lembre-se!!!
+ * Não se pode criar variaveis com numero iniciando! O primeiro caracter tem que ser uma letra
+ * Utilizamos camelCase - palavras compostas: nomeCliente
+ * Case-Sensitive - Letras maiúsculas fazem diferença! nomeCliente é diferente de nomecliente
+ * Não pode redeclarar variáveis
+ * Não utilize var! Utilize let se algo vai ficar variando no seu código! 
+ */
+
+console.log(nome, "nasceu em 1999");
+console.log(nome2, "Teve um filho!")
