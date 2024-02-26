16fd66561bc37bbf82038f52a0202d7c584958bc
Wesley Moraes <wesleycrz@hotmail.com>
2024-02-26 14:05:47 -0300

    comentarios

---
+++ s2-js-basico/aula04/comentarios.js
@@ -0,0 +1,14 @@
+/*
+São textos que são ignorados 
+
+*/
+
+console.log("Exibe")
+//console.log("Não exibe");
+
+console.log("Exibe novamente")
+/**
+ * console.log("Bloco que não se exibe!")
+ * console.log("Bloco que não se exibe!")
+ * console.log("Bloco que não se exibe!")
+ */
