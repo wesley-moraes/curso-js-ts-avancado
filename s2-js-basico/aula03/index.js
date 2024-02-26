0d97bb4f2d5e802154a84917ac99a447a47845a8
Wesley Moraes <wesleycrz@hotmail.com>
2024-02-26 13:58:29 -0300

    organizando as aulas e console.log

---
+++ s2-js-basico/aula03/index.js
@@ -0,0 +1,22 @@
+/*
+Console.log():
+É um objeto (console) que tem uma função que se chama log().
+É usado para debuagem de código!
+
+
+String = Texto
+
+'' -> Querer utilizar aspas no seu texto, 
+"" -> Também serve para usar aspas
+`` -> Template String, com crases!
+
+JS é uma lingaugem de tipagem dinâmica!
+
+*/
+
+console.log(" 'Wesley' Moraes ");
+console.log(' Wesley "Moraes" ');
+console.log(` "Wesley" 'Moraes' `);
+console.log(31, 13.33, "Wesley dos Santos Moraes");
+
+console.log('Meu nome é "Wesley". Estou aprendendo javascript há', 10 , 'anos');
