641f885c5179db7749c7bdbe73fb29e9eb7b4a67
Wesley Moraes <wesleycrz@hotmail.com>
2024-02-27 12:59:57 -0300

    operadores logicos

---
+++ s3-logica-programacao/aula21/index.js
@@ -0,0 +1,17 @@
+/**
+ * Operadores de Comparação:
+ * > Maior que
+ * >= Maior igual que
+ * < Menor que
+ * <= Menor igual que
+ * == igualdade (valor)
+ * === igualdade estrita (valor e tipo)
+ * != diferente de (valor)
+ * !== diferente estrito (valor e tipo)
+ */
+
+console.log(10 > 5);
+console.log(10 == "10"); //Comparação de String com Number
+console.log(10 === "10"); // Comparação de String com Number
+console.log(10 !== "10");
+console.log(10 != "10");
