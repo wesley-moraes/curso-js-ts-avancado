125906fa9296485b0432fadda6e4eb0ebd0c2740
Wesley Moraes <wesleycrz@hotmail.com>
2024-02-26 22:40:53 -0300

    numeros

---
+++ s2-js-basico/aula14/index.js
@@ -0,0 +1,13 @@
+let num1 = 10.44545; //number
+let num2 = 2.5; //number
+
+//console.log(num1.toString() + num2); //Ele transforma tudo em string;
+//console.log(typeof num1.toString());
+
+//console.log(num1.toString(2)); //Mostra o valor em binarios
+console.log(num1.toFixed(2)); //restringe a quantidade de numeros a serem exibidos.
+
+console.log(Number.isInteger(num1)); //Retorna um boolean
+
+//console.log(25+16+25+28+1+25+20+19+36)
+//console.log(196/60);
