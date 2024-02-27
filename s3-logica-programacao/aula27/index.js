28805c0436998acfc05675b2debf7e6e85744de2
Wesley Moraes <wesleycrz@hotmail.com>
2024-02-27 15:22:17 -0300

    operacao ternaria

---
+++ s3-logica-programacao/aula27/index.js
@@ -0,0 +1,5 @@
+//Operação Ternaria - ? :
+//Verificação para usuário vip! Se for maior que mil, é vip, senão é falso
+let pontuacaoUsuario = 300
+const usuarioVip = pontuacaoUsuario >= 1000 ? "VIP" : "Não é VIP";
+console.log(usuarioVip);
