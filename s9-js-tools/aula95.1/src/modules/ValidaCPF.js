7cab85d7eb91262b60bee1644f1b3f7a55fb1e5c
Wesley Moraes <wesleycrz@hotmail.com>
2024-03-13 20:36:30 -0300

    gerador de cpf

---
+++ s9-js-tools/aula95.1/src/modules/ValidaCPF.js
@@ -0,0 +1,58 @@
+export default class ValidaCPF{
+    constructor(cpfEnviado){
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: true,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        });
+    }
+
+    eSequencia(){
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    }
+
+    geraNovoCpf(){
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2;
+    }
+
+    static geraDigito(cpfSemDigitos){
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for(let stringNumerica of cpfSemDigitos){
+            total += reverso * Number(stringNumerica);
+            reverso--;
+            //console.log(stringNumerica, typeof stringNumerica);
+        }
+
+        const digito =  11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
+        
+    }
+
+    valida(){
+        if(!this.cpfLimpo) return false;
+        if(typeof this.cpfLimpo !== 'string') return false;
+        if(this.cpfLimpo.length !== 11) return false;
+        if(this.eSequencia()) return false;
+        this.geraNovoCpf();
+
+        return this.novoCPF === this.cpfLimpo;
+    }
+}
+
+/*
+const validaCPF = new ValidaCPF('070.987.720-03');
+//const validaCPF = new ValidaCPF('999.999.799-99');
+if(validaCPF.valida()){
+    console.log('CPF VALIDO');
+}else{
+    console.log('CPF INVÁLIDO');
+}
+*/
+
+console.log('Cheguei aqui');
