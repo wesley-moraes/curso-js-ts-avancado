2622759d3a46c470fdd638b68900425546fc0380
Wesley Moraes <wesleycrz@hotmail.com>
2024-03-16 01:29:54 -0300

    express views

---
+++ s10-node-npm-express-mongoDB/aula10/server.js
@@ -0,0 +1,59 @@
+//MONTANDO UM SERVIDOR NODE!
+
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+const path = require('path');
+
+//http://facebook.com/profiles/
+//URL Params!
+//
+//Params -> Partes que vem na rota da url (tipo usuario1)
+//Query -> QueryString. Algoq ue vem na barra mesmo. (tipo /?chave=valor1&chave2=valor2)
+
+
+//Isso aqui é para permitir o uso do POST.
+//Para real envio de formulário
+//Isso aqui trata o Post! Se não usar, vai dar como undefined
+app.use(
+    express.urlencoded(
+        {
+            extended: true
+        }
+    )
+)
+
+//Views
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(routes); //Aqui é para usar as rotas! Deixar visível para ser usado
+
+/*
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+            Nome do Cliente: <input type="text" name="nome"> <br>
+            Outro Campo: <input type="text" name="outroCampo" /> <br>
+            <button>Enviar Form</button>
+        </form>
+    `);
+});
+*/
+
+/*
+app.get('/testes/:idUsuarios?/:parametro?', (req, res)=>{
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.query.facebookprofile);
+});
+
+app.post('/', (req, res) =>{
+    console.log(req.body)
+    res.send(`O que você me enviou foi: ${req.body.nome}`);
+})
+*/
+
+app.listen(3000);
+
---
+++ s10-node-npm-express-mongoDB/aula10/src/controllers/homeController.js
@@ -0,0 +1,18 @@
+
+
+exports.paginaInicial = (req, res) => {
+    /*
+    res.send(`
+        <form action="/" method="POST">
+            Nome do Cliente: <input type="text" name="nome"> <br>
+            Outro Campo: <input type="text" name="outroCampo" /> <br>
+            <button>Enviar Form</button>
+        </form>
+    `);
+    */
+   res.render('index');
+}
+
+exports.trataPost = (req, res) =>{
+    res.send('Nova Rota de Post');
+}
