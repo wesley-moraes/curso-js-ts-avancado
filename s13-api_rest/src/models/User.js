ffd382a3f4ce3d46eb02c7c942bd59d2afdf35c2
Wesley Moraes <wesleycrz@hotmail.com>
2024-04-29 16:20:23 -0300

    parte do projeto

---
+++ s13-api_rest/src/models/User.js
@@ -0,0 +1,49 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres'
+          }
+        }
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'e-mail inválido'
+          }
+        }
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'A senha deve ter entre 6 e 50 caracteres'
+          }
+        }
+      }
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async user => {
+      user.password_hash = await bcryptjs.hash(password, 8);
+    }); //Execute antes de salvar
+
+    return this
+  }
+}
