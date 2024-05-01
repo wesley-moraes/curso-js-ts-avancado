619db09ee30aba7af430560a727e23f637fe6136
Wesley Moraes <wesleycrz@hotmail.com>
2024-05-01 20:28:18 -0300

    crus usuarios

--- s13-api_rest/src/controllers/UserController.js
+++ s13-api_rest/src/controllers/UserController.js
@@ -12,16 +12,89 @@ class UserController {
         }
         */
       )
-      res.json({
+      return res.json({
         novoUser
       });
     } catch (e) {
       console.log(e);
-      res.status(400).json({
+      return res.status(400).json({
         erros: e.errors.map((err) => err.message)
       });
     }
 
+  }
+
+  //Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  //Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id); //Primary key
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  //Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado']
+        })
+      }
+
+      const user = await User.findByPk(req.params.id); //Primary key
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe']
+        })
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message)
+      });
+    }
+  }
+
+  //Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado']
+        })
+      }
+
+      const user = await User.findByPk(req.params.id); //Primary key
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe']
+        })
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message)
+      });
+    }
   }
 }
 
--- s13-api_rest/src/models/User.js
+++ s13-api_rest/src/models/User.js
@@ -45,7 +45,10 @@ export default class User extends Model {
     });
 
     this.addHook('beforeSave', async user => {
-      user.password_hash = await bcryptjs.hash(user.password, 8);
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+
     }); //Execute antes de salvar
 
     return this
