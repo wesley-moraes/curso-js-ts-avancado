e958dd3b482dd7e62913dc37a0e2875b23e9c8c3
wesley-moraes <wesleycrz@hotmail.com>
2024-08-12 17:03:07 -0300

    add arquivo

--- s13-api_rest/src/controllers/AlunoController.js
+++ s13-api_rest/src/controllers/AlunoController.js
@@ -5,6 +5,102 @@ class AlunoController{
     const alunos = await Aluno.findAll()
     res.json(alunos);
   }
+
+  async store(req, res){
+    try{
+      const aluno = await Aluno.create(req.body);
+
+      res.json(aluno);
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+
+
+  }
+
+  async show(req, res){
+    try{
+      const {id} = req.params;
+      if(!id){
+        return res.status(400).json({
+          errors: ['Faltando ID']
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+      if(!aluno){
+        return res.status(400).json({
+          errors: ['Aluno não existe']
+        });
+      }
+
+      res.json(aluno);
+
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+  }
+
+  async delete(req, res){
+    try{
+      const {id} = req.params;
+      console.log('o id do aluno:' , id)
+      if(!id){
+        return res.status(400).json({
+          errors: ['Faltando ID']
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+      console.log('Dados do aluno', aluno)
+      if(!aluno){
+        return res.status(400).json({
+          errors: ['Aluno não existe']
+        });
+      }
+
+      await aluno.destroy();
+      res.json({
+        "apagado" : true
+      })
+
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+  }
+
+  async update(req, res){
+    try{
+      const {id} = req.params;
+      if(!id){
+        return res.status(400).json({
+          errors: ['Faltando ID']
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+      if(!aluno){
+        return res.status(400).json({
+          errors: ['Aluno não existe']
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      res.json(alunoAtualizado);
+
+    }catch(e){
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      });
+    }
+  }
 }
 
 export default new AlunoController();
---
+++ s13-api_rest/src/routes/fotoRoutes.js
@@ -0,0 +1,16 @@
+//import express from 'express';
+//const router = new express.Router(); //É uma maneira de fazer o import.
+
+import { Router } from 'express';
+import multer from 'multer';
+
+import fotoController from '../controllers/FotoController';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig);
+
+const router = new Router();
+
+router.post('/', upload.single('foto'), fotoController.store)
+
+export default router;
